c44483bb98f851a20268af360aaf7c8a742d1074
wang3450 <wang3450@purdue.edu>
2022-09-26 15:00:05 -0400

    All Images and Code finished, just need report

--- HW04/run_harris.sh
+++ HW04/run_harris.sh
@@ -8,57 +8,32 @@ NCC="NCC"
 SSD="SSD"
 
 # Book Image Set:
-# Sigma = 5
-python3 $harris $book 5 $NCC
-python3 $harris $book 5 $SSD
-
-# Book Image Set:
-# Sigma = 10
-python3 $harris $book 10 $NCC
-python3 $harris $book 10 $SSD
-
+# Sigma = 1.6, 2
+python3 $harris $book 1.6 $NCC
+python3 $harris $book 1.6 $SSD
+python3 $harris $book   2 $NCC
+python3 $harris $book   2 $SSD
 
 # Fountain Image Set:
-# Sigma = 1.2
-python3 $harris $fountain 1.2 $NCC
-python3 $harris $fountain 1.2 $SSD
-
-# Fountain Image Set:
-# Sigma = 5
-python3 $harris $fountain 5 $NCC
-python3 $harris $fountain 5 $SSD
-
-# Fountain Image Set:
-# Sigma = 10
-python3 $harris $fountain 10 $NCC
-python3 $harris $fountain 10 $SSD
+# Sigma = 1.6, 2
+python3 $harris $fountain 1.6 $NCC
+python3 $harris $fountain 1.6 $SSD
+python3 $harris $fountain   2 $NCC
+python3 $harris $fountain   2 $SSD
 
 # Checkerboard Image Set:
-# Sigma = 1.2
-python3 $harris $checkerboard 1.2 $NCC
-python3 $harris $checkerboard 1.2 $SSD
-
-# Checkerboard Image Set:
-# Sigma = 5
-python3 $harris $checkerboard 5 $NCC
-python3 $harris $checkerboard 5 $SSD
-
-# Checkerboard Image Set:
-# Sigma = 10
-python3 $harris $checkerboard 10 $NCC
-python3 $harris $checkerboard 10 $SSD
+# Sigma = 1.6, 2
+python3 $harris $checkerboard 1.6 $NCC
+python3 $harris $checkerboard 1.6 $SSD
+python3 $harris $checkerboard   2 $NCC
+python3 $harris $checkerboard   2 $SSD
 
 # Locker Image Set:
-# Sigma = 1.2
-python3 $harris $locker 1.2 $NCC
-python3 $harris $locker 1.2 $SSD
+# Sigma = 1.6, 2
+python3 $harris $locker 1.6 $NCC
+python3 $harris $locker 1.6 $SSD
+python3 $harris $locker   2 $NCC
+python3 $harris $locker   2 $SSD
 
-# Locker Image Set:
-# Sigma = 5
-python3 $harris $locker 5 $NCC
-python3 $harris $locker 5 $SSD
 
-# Locker Image Set:
-# Sigma = 10
-python3 $harris $locker 10 $NCC
-python3 $harris $locker 10 $SSD
+
