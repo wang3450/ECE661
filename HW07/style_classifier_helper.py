f2b6c9433565aaeeff63cd5d9c1f3820f059ccd2
Joseph Wang <wang3450@purdue.edu>
2022-10-26 15:30:14 -0400

    Working VGG19 based Texture Classifier

---
+++ HW07/style_classifier_helper.py
@@ -0,0 +1,109 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import csv
+from multiprocessing import pool
+import os
+import cv2
+
+'''VGG19(nn.Module)
+Input: (256 x 256) image tensor
+Output: (512 x 16 x 16) image tensor
+Purpose: given an image tensor, extract feature map'''
+class VGG19(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            # encode 1-1
+            nn.Conv2d(3, 3, kernel_size=(1, 1), stride=(1, 1)),
+            nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),  # relu 1-1
+            # encode 2-1
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),  # relu 2-1
+            # encoder 3-1
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),  # relu 3-1
+            # encoder 4-1
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),  # relu 4-1
+            # rest of vgg not used
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            nn.ReLU(inplace=True),  # relu 5-1
+            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            # nn.ReLU(inplace=True),
+            # nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect'),
+            # nn.ReLU(inplace=True)
+        )
+
+    def load_weights(self, path_to_weights):
+        vgg_model = torch.load(path_to_weights)
+        # Don't care about the extra weights
+        self.model.load_state_dict(vgg_model, strict=False)
+        for parameter in self.model.parameters():
+            parameter.requires_grad = False
+
+    def forward(self, x):
+        # Input is numpy array of shape (H, W, 3)
+        # Output is numpy array of shape (N_l, H_l, W_l)
+        x = torch.from_numpy(x).permute(2, 0, 1).unsqueeze(0).float()
+        out = self.model(x)
+        out = out.squeeze(0).numpy()
+        return out
+
+
+'''loadImages
+Input: file directory
+Output: 2 lists
+Purpose: read all train and test images'''
+def loadImages(dir_path):
+    img_list = list()
+    label_list = list()
+    for filename in os.listdir(dir_path):
+        filepath = os.path.join(dir_path, filename)
+        if os.path.isfile(filepath) and ('.jpg' in filepath or '.jpeg' in filepath):
+            img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
+            resize_img = cv2.resize(img, (256, 256), cv2.INTER_AREA)
+            assert(resize_img.shape == (256, 256, 3))
+            img_label = -1
+            if 'cloudy' in filename:
+                img_label = 0
+            elif 'rain' in filename:
+                img_label = 1
+            elif 'shine' in filename:
+                img_label = 2
+            elif 'sunrise' in filename:
+                img_label = 3
+            try:
+                assert (img_label >= 0)
+                img_list.append(resize_img)
+                label_list.append(img_label)
+            except AssertionError:
+                pass
+    return img_list, label_list
