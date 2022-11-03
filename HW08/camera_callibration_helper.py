e9f3186ead920683d4fafed943cb0abb0fe00782
Joseph Wang <wang3450@purdue.edu>
2022-11-03 19:54:27 -0400

    working on hough lines clustering

--- HW08/camera_callibration_helper.py
+++ HW08/camera_callibration_helper.py
@@ -7,15 +7,16 @@ Input: directory path
 Output: list of grey scale images and labels
 Purpose: given directory path, load images and labels"""
 def loadImages(dir_path):
-    img_list = list()
-    label_list = list()
+    raw_img_list = list()
+    grey_img_list = list()
     for filename in os.listdir(dir_path):
         filepath = os.path.join(dir_path, filename)
         if os.path.isfile(filepath) and ('.jpg' in filepath or '.jpeg' in filepath):
-            img = cv2.imread(filepath, 0)
-            img_list.append(img)
-            label_list.append(filename)
-    return img_list, label_list
+            raw_img = cv2.imread(filepath)
+            grey_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
+            raw_img_list.append(raw_img)
+            grey_img_list.append(grey_img)
+    return raw_img_list, grey_img_list
 
 
 """performCanny(grey_img_list)
@@ -25,6 +26,18 @@ Purpose: Given a list of grey scale images, apply canny on them"""
 def performCanny(grey_img_list):
     edge_img_list = list()
     for img in grey_img_list:
-        edge = cv2.Canny(img, 255, 355)
+        edge = cv2.Canny(img, 300, 300)
         edge_img_list.append(edge)
     return edge_img_list
+
+
+"""performHoughTransform(edge_img_list)
+Input: list of edge maps
+Output: list of hough lines
+Purpose: Given a list of edge maps, return a list of hough lines"""
+def performHoughTransform(edge_img_list):
+    hough_lines_list = list()
+    for img in edge_img_list:
+        line = cv2.HoughLines(img, 1, np.pi/180, 52)
+        hough_lines_list.append(line)
+    return hough_lines_list
